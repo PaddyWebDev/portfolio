bda29caec56de5a609864c026d68e29245e94d37
Padmanabh Malwade <101687265+PaddyWebDev@users.noreply.github.com>
2024-04-30 18:00:15 +0530

    Updated HeadSection.tsx

--- src/pages/components/HeadSection.tsx
+++ src/pages/components/HeadSection.tsx
@@ -5,7 +5,7 @@ export default function HeadSection() {
     return (
         <Head>
             <title>
-             Padmanabh's Full-Stack Showcase: Crafting Web Experiences with Java, JavaScript, and Next.js
+                {"Padmanabh's Full-Stack Showcase: Crafting Web Experiences with Java, JavaScript, and Next.js"}
             </title>
             <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         </Head>
